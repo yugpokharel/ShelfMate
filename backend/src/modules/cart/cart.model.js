73d89cf7aee4ec97a6444fc57415c865151c59e0
yugpokharel <yugpokharel1@gmail.com>
2026-07-02 14:09:14 +0545

    feat(cart): add Cart model

---
+++ backend/src/modules/cart/cart.model.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const cartItemSchema = new mongoose.Schema(
+  {
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+      min: 1,
+      default: 1,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+  },
+  { _id: true }
+);
+
+const cartSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+      unique: true,
+      index: true,
+    },
+    items: {
+      type: [cartItemSchema],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: (_, ret) => {
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+cartSchema.index({ user: 1 });
+
+module.exports = mongoose.model('Cart', cartSchema);
