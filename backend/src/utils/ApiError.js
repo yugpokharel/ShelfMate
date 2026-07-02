bf76c68bbd2459d62af3e89cd7e33c039af2ef67
yugpokharel <yugpokharel1@gmail.com>
2026-07-02 14:12:10 +0545

    chore: add ApiResponse and ApiError utility classes

---
+++ backend/src/utils/ApiError.js
@@ -0,0 +1,12 @@
+class ApiError extends Error {
+  constructor(statusCode, message, errors = [], stack = '') {
+    super(message);
+    this.statusCode = statusCode;
+    this.errors = errors;
+    this.success = false;
+    if (stack) this.stack = stack;
+    else Error.captureStackTrace(this, this.constructor);
+  }
+}
+
+module.exports = ApiError;
